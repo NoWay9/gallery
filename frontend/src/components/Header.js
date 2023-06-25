8cb7c0f741fff26c0fa09cea96fa72f7f85c7a43
Liubomyr Struk <strukliubomyr@gmail.com>
2023-06-25 13:08:55 +0300

    Search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
+const navBarStyle= {
+  backgroundColor: 'lightblue'
+}
+
 const Header = ({title}) => {
     return (
-        <Navbar bg="light" data-bs-theme="light">
+        <Navbar style={navBarStyle} data-bs-theme="light">
         <Container>
           <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Container>
