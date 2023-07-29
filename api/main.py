68e813dfb32209a59c8cb9df358013b488d461ce
Liubomyr Struk <strukliubomyr@gmail.com>
2023-07-29 13:52:25 +0300

    enabled pylint

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
-import requests
 import os
+import requests
+from flask import Flask, request
 from dotenv import load_dotenv
 from flask_cors import CORS
 
@@ -11,25 +11,24 @@ UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
+    raise EnvironmentError(
+        "Please create .env.local file and insert there UNSPLASH_KEY"
+    )
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
-    params = {
-        "query": word
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     return {"data": response.json()}
 
-if __name__  == "__main__":
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
